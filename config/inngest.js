5a6a9c2861a337918eb6f85737b579cb5038e87d
Hiếu Nguyễn <ngvannhieu2005@gmail.com>
2025-03-23 12:38:01 +0700

    update

--- config/inngest.js
+++ config/inngest.js
@@ -56,7 +56,7 @@ export const createUserOrder = inngest.createFunction(
   { 
     id: 'create-user-order',
     batchEvents: {
-      maxEvents: 25,
+      maxEvents: 5,
       timeout: '5s'
     }
   },
