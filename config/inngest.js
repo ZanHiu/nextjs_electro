698f0b7cdce09d733db95b472880fd501635666b
Hiếu Nguyễn <ngvannhieu2005@gmail.com>
2025-04-21 13:40:34 +0700

    fix bug

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
-import connectDB from "./db";
-import User from "@/models/User";
+import connectDB from "@/lib/db";
+import User from "@/models/user";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "electro-next" });
