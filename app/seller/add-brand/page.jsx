5ecc9ad2e8f27e14d1ba87419ae4bd8ffd2a3d38
long981 <longstar981@gmail.com>
2025-06-25 11:00:49 +0700

    add payment option, all brands page & add/edit brand form

---
+++ app/seller/add-brand/page.jsx
@@ -0,0 +1,131 @@
+'use client'
+import React, { useState } from "react";
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import { useAppContext } from "@/context/AppContext";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const AddBrand = () => {
+  const { getToken } = useAppContext();
+
+  const [files, setFiles] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [views, setViews] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("name", name);
+    formData.append("description", description);
+    formData.append("views", views);
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append(`images`, files[i]);
+    }
+
+    try {
+      const token = await getToken();
+      const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/brands/add`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      })
+      if (data.success) {
+        toast.success(data.message);
+        setFiles([]);
+        setName('');
+        setDescription('');
+        setViews('');
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <div className="flex-1 h-screen overflow-scroll flex flex-col justify-between text-sm">
+      <form onSubmit={handleSubmit} className="md:p-10 p-4 space-y-5 max-w-lg">
+        <h1 className="text-2xl font-semibold mb-6">Add Brand</h1>
+        <div className="flex flex-col gap-1 max-w-md">
+          <label className="text-base font-medium" htmlFor="brand-name">
+            Brand Name
+          </label>
+          <input
+            id="brand-name"
+            type="text"
+            placeholder="Type here"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            required
+          />
+        </div>
+        <div>
+          <p className="text-base font-medium">Brand Image</p>
+          <div className="flex flex-wrap items-center gap-3 mt-2">
+            {[...Array(4)].map((_, index) => (
+              <label key={index} htmlFor={`image${index}`}>
+                <input onChange={(e) => {
+                  const updatedFiles = [...files];
+                  updatedFiles[index] = e.target.files[0];
+                  setFiles(updatedFiles);
+                }} type="file" id={`image${index}`} hidden />
+                <Image
+                  key={index}
+                  className="max-w-24 cursor-pointer"
+                  src={files[index] ? URL.createObjectURL(files[index]) : assets.upload_area}
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+              </label>
+            ))}
+          </div>
+        </div>
+<div className="flex flex-col gap-1 max-w-md">
+          <label
+            className="text-base font-medium"
+            htmlFor="brand-description"
+          >
+            Brand Description
+          </label>
+          <textarea
+            id="brand-description"
+            rows={4}
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none"
+            placeholder="Type here"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            required
+          ></textarea>
+        </div>
+        <div className="flex flex-col gap-1">
+          <label className="text-base font-medium" htmlFor="views">
+            Views
+          </label>
+          <input
+            id="views"
+            type="number"
+            placeholder="0"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+            onChange={(e) => setViews(e.target.value)}
+            value={views}
+            required
+          />
+        </div>
+        <button type="submit" className="px-8 py-2.5 bg-orange-600 text-white font-medium rounded">
+          ADD
+        </button>
+      </form>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default AddBrand;
